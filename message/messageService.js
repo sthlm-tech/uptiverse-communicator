cf28777450e0529480a02f2d5c150b8c629cd85e
ulfdahlstrand <ulf.d.dahlstrand@gmail.com>
2018-02-28 21:15:37 +0100

    moved storing of messages to own method and cleans up code

--- message/messageService.js
+++ message/messageService.js
@@ -13,38 +13,39 @@ function MessageService() {
 		.then(function(list){
 			for(var i = 0; i < list.length;i++){
 				var item = list[i];
-				var url = item.url;
-				var data = input.data;
-				request
-				 .post(url)
-				 .send(data)
-				 .set('Authorization', token)
-				 .end(function(err, response){
-					 var message = new Message();
-
-					 message.message = input.message;
-					 message.scope = input.scope;
-					 message.token = token;
-					 message.listener = item;
-					 message.data = data;
-					 message.sucessfullySent = response.ok;
-					 message.created = new Date();
-
-					 message.save(function(err, createdMessage){
-						 if(err){console.log(err);}
-						 deferred.resolve();
-					 });
-
-					});
-
+				storeMessage(item, input, token, function(){
+					deferred.resolve();
+				});
 			}
 
 		});
-		//deferred.resolve();
 		return deferred.promise;
 	}
 
+function storeMessage(listener, input, token, callback){
 
+	request
+	 .post(listener.url)
+	 .send(input.data)
+	 .set('Authorization', token)
+	 .end(function(err, response){
+		 var message = new Message();
+
+		 message.message = input.message;
+		 message.scope = input.scope;
+		 message.token = token;
+		 message.listener = listener;
+		 message.data = input.data;
+		 message.sucessfullySent = response.ok;
+		 message.created = new Date();
+
+		 message.save(function(err, createdMessage){
+			 if(err){console.log(err);}
+			 callback();
+		 });
+
+		});
+}
 
 }
 module.exports = new MessageService();
